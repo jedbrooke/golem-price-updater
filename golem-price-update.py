dd15695b68f7efbb0202b254845b87a7970975e3
Jasper Edbrooke <jasperedbrooke@gmail.com>
2021-12-01 16:38:10 -0800

    add deafualts to help message, update -t behavior

--- golem-price-update.py
+++ golem-price-update.py
@@ -8,14 +8,21 @@ import time
 
 def parse_args():
     parser = argparse.ArgumentParser(description="automatically adjust golem provider prices")
-    parser.add_argument("--currency",type=str,help="desired fiat currency for prices",default="USD")
+    parser.add_argument("--currency",type=str,help="desired fiat currency for prices, default: USD",default="USD")
     
     # TODO: if there's some kind of API for getting your local electricty costs feel free to submit a PR
-    parser.add_argument("--price_kwh",type=float,help="electricity price per kwH",default=0.1)
-    parser.add_argument("-w","--watts",type=int,help="power usage of your provider in watts",default=150)
+    parser.add_argument("--price_kwh",type=float,help="electricity price per kwH in your fiat currency, default: 0.1",default=0.1)
+    parser.add_argument("-w","--watts",type=int,help="power usage of your provider in watts, default: 150",default=150)
     # TODO: add option to target earnings that could have been made by mining a coin/other workload
-    parser.add_argument("-p","--profit",type=float,help="desired additional profit beyond electricity costs, (1.0 is 100 percent)",default=0.1)
-    parser.add_argument("-t","--threads",type=int,help="number of threads on golem provider (default is all threads)",default=os.cpu_count())
+    parser.add_argument("-p","--profit",type=float,help="desired additional profit beyond electricity costs, (1.0 is 100 percent), default: 0.1 (10 percent)",default=0.1)
+    parser.add_argument("-t","--threads",type=int,help='''number of threads on golem provider. \n
+    t = 0 is all threads. \n
+    t < 0 is t less that all threads. \n
+    t > 1 is t threads. \n
+    if t is higher than available threads, maximum of all threads will be used. \n
+    if all threads - t is less than 1, 1 thread will be used. \n
+    default: all threads''',default=0)
+    parser.add_argument("--dry_run",action="store_true",help="display result but do not actually update")
 
     # TODO: add option to go based off of prices of similar nodes instead of electricty cost
 
@@ -29,14 +36,23 @@ if __name__ == "__main__":
     json_data = request.json()
     glm_price = json_data["golem"][args.currency.lower()]
 
+    num_threads = os.cpu_count()
+    if args.threads > 0:
+        num_threads = min(os.cpu_count(),args.threads)
+    if args.threads < 0:
+        num_threads = max(os.cpu_count() + args.threads,1)
+
     cost_per_hour = (args.watts * 0.001) * args.price_kwh
     desired_income_per_hour = cost_per_hour * (1 + args.profit)
-    price_per_thread_fiat = desired_income_per_hour / args.threads
+    price_per_thread_fiat = desired_income_per_hour / num_threads
     price_per_thread_glm = price_per_thread_fiat * (1 / glm_price)
-    # set the price
-    golem_path = os.path.expanduser("~/.local/bin/golemsp")
-    subprocess.call([golem_path,"settings","set","--cpu-per-hour",str(price_per_thread_glm)])
-    subprocess.call([golem_path,"settings","set","--env-per-hour","0"])
-    subprocess.call([golem_path,"settings","set","--starting-fee","0"])
+    if args.dry_run:
+        print("CPU per hour:",price_per_thread_glm)
+    else:
+        # set the price
+        golem_path = os.path.expanduser("~/.local/bin/golemsp")
+        subprocess.call([golem_path,"settings","set","--cpu-per-hour",str(price_per_thread_glm)])
+        subprocess.call([golem_path,"settings","set","--env-per-hour","0"])
+        subprocess.call([golem_path,"settings","set","--starting-fee","0"])
 
 
