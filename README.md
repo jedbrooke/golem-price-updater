09a33330eea8df479d97243b783514f7aa347718
Jasper Edbrooke <jasperedbrooke@gmail.com>
2021-12-27 17:41:35 -0800

    add update crontab functionality

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ set this file to run as a cron job every hour (or as often as you see fit)
 1. make the file executable
 ```chmod +x golem-price-update.py```
 
-1.  add it to your crontab (with `crontab -e`)
+1. run the program with your settings, and `-u` or `--update_cron` to add it to your crontab to run automatically. (requires the `python-crontab` module)
+    example: `python3 golem-price-update.py -w 100 -p 0.5 --update_cron`
+
+
+    you can also manually add it to your crontab (with `crontab -e`)
 
     ```0 * * * * /path/to/golem-price-update.py > /dev/null```
 
@@ -15,8 +19,9 @@ set this file to run as a cron job every hour (or as often as you see fit)
     ```0 * * * * /path/to/golem-price-update.py --currency EUR --watts 250 --price_kwh 0.17 > /dev/null```
 
 for a full list of options see `golem-price-update.py --help`
+
 ```
-usage: golem-price-update.py [-h] [--currency CURRENCY] [--price_kwh PRICE_KWH] [-w WATTS] [-p PROFIT] [-t THREADS] [--dry_run]
+usage: golem-price-update.py [-h] [--currency CURRENCY] [--price_kwh PRICE_KWH] [-w WATTS] [-p PROFIT] [-t THREADS] [-u] [--dry_run]
 
 automatically adjust golem provider prices. GLM Price provided by CoinGecko
 
@@ -32,7 +37,8 @@ optional arguments:
   -t THREADS, --threads THREADS
                         number of threads on golem provider. t = 0 is all threads. t < 0 is t less that all threads. t > 1 is t threads. if t is higher than available threads, maximum of all
                         threads will be used. if all threads - t is less than 1, 1 thread will be used. default: all threads
-  --dry_run             display result but do not actually update
+  -u, --update_cron     run this script every hour with the same settings by adding it to the cron file
+  --dry_run             display result but do not actually update, (applied to cron setting as well)
 ```
 
 
--- golem-price-update.py
+++ golem-price-update.py
@@ -6,6 +6,7 @@ import subprocess
 import sys
 import time
 import shlex
+import random
 
 def parse_args():
     parser = argparse.ArgumentParser(description="automatically adjust golem provider prices. GLM Price provided by CoinGecko")
@@ -23,15 +24,58 @@ def parse_args():
     if t is higher than available threads, maximum of all threads will be used. \n
     if all threads - t is less than 1, 1 thread will be used. \n
     default: all threads''',default=0)
-    parser.add_argument("--dry_run",action="store_true",help="display result but do not actually update")
+    parser.add_argument("-u","--update_cron",action="store_true",help="run this script every hour with the same settings by adding it to the cron file")
+    parser.add_argument("--dry_run",action="store_true",help="display result but do not actually update, (applied to cron setting as well)")
 
     # TODO: add option to go based off of prices of similar nodes instead of electricty cost
 
     return parser.parse_args()
 
 
+def print_arg(k,v) -> str:
+    if type(v) is bool:
+        if v:
+            return f"--{k}"
+        else:
+            return ""
+    return f"--{k} {v}"
+
 if __name__ == "__main__":
     args = parse_args()
+    if args.update_cron:
+        try:
+            from crontab import CronTab
+            from crontabs import CronTabs
+        except ModuleNotFoundError:
+            print("")
+            print("module `python-crontab` is required to update the crontab, it can be installed with:")
+            print("")
+            print("pip3 install python-crontab")
+            print("")
+            exit(0)
+
+        args_dict = vars(args)
+        del args_dict["update_cron"]
+        cmd = f"{os.path.abspath(__file__)} {' '.join([print_arg(k,v) for k,v in args_dict.items()])} > /dev/null 2>&1"
+        comment = "update GLM price, set by script"
+        cron = CronTab(user=True)
+        job = cron.new(command=cmd,comment=comment)
+        # TODO make this settable by an arg
+        job.minute.on(random.randint(0,59))
+        job.hour.every(1)
+        if args.dry_run:
+            print("removing the following jobs from crontab")
+            print(*cron.find_comment(comment),sep="\n")
+            print("adding the following to cron tab:")
+            print(job)
+        else:
+            # remove old job
+            cron.remove_all(comment=comment)
+            cron.append(job)
+            cron.write()
+
+            
+
 
     request = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids=golem&vs_currencies={args.currency}")
     json_data = request.json()
@@ -52,6 +96,7 @@ if __name__ == "__main__":
     else:
         # set the price
         golem_path = os.path.expanduser("~/.local/bin/golemsp")
+        
         # broken in yagna 0.9.1
         # cmd = f"{golem_path} preset update --no-interactive --price Duration=0 --price CPU={price_per_thread_glm/3600} --pricing linear vm"
         # print(cmd)
@@ -60,7 +105,7 @@ if __name__ == "__main__":
         # subprocess.call(shlex.split(cmd))
         
         
-        # fixed in yagan 0.9.1
+        # fixed in yagna 0.9.1
         subprocess.call([golem_path,"settings","set","--cpu-per-hour",str(price_per_thread_glm)])
         subprocess.call([golem_path,"settings","set","--env-per-hour","0"])
         subprocess.call([golem_path,"settings","set","--starting-fee","0"])
